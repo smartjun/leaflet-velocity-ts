02476481a9858b2a5fc9cbc5072749129eefe156
阮细浩 <rxh12352@gmail.com>
2019-01-24 19:17:58 +0800

    fix: 更换一个参数

--- src/layer.ts
+++ src/layer.ts
@@ -71,7 +71,7 @@ export default class layer {
      */
     distortion (λ: number, φ: number, x: number, y: number): number[] {
         const τ = 2 * Math.PI;
-        const H = Math.pow(10, -5.2);
+        const H = 5;
         const hλ = λ < 0 ? H : -H;
         const hφ = φ < 0 ? H : -H;
 
